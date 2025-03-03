334a95aa06a24fe5383f9ce982f40d8f4d6b914b
Joan Nyawira Kori <korijoan171@gmail.com>
2025-03-03 22:31:04 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Joan Kori";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;/*string interpolation*/
+let excitedWelcomeMessage =`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} !`;
+let shortGreeting = `Welcome, ${currentUser[0]} !`;/*used an array to get the first letter of the
+first name in currentUser*/
